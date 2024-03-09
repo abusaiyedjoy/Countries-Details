8dd2a59c8ca753721cb5ece1e93feb174eacea35
abusaiyedjoy <abusaiyedjoy1@gmail.com>
2024-03-09 11:46:24 +0600

    Add country name & flag

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countries from './Components/Countries/countries'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Practice</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
